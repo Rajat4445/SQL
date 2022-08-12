bf2245bb81711d7cda69eda96615b1a1c7670fa2
Rajat Bahuguna <rajatbahuguna4445@gmail.com>
2022-08-12 07:02:52 +0530

    Update SQL_leetcode.sql

--- SQL_leetcode.sql
+++ SQL_leetcode.sql
@@ -27,8 +27,90 @@ FROM person
 GROUP BY email
 HAVING COUNT(email) > 1
 
+------------------------------------------------------------------------------------------------------------------------------------------------------
+
+/*
+Table: Customers
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| name        | varchar |
++-------------+---------+
+id is the primary key column for this table.
+Each row of this table indicates the ID and name of a customer.
+ 
+
+Table: Orders
+
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| id          | int  |
+| customerId  | int  |
++-------------+------+
+id is the primary key column for this table.
+customerId is a foreign key of the ID from the Customers table.
+Each row of this table indicates the ID of an order and the ID of the customer who ordered it.
+ 
+
+Write an SQL query to report all customers who never order anything.
+
+Return the result table in any order.        */
 
 
+SELECT name AS customers FROM customers
+WHERE id NOT IN (SELECT DISTINCT(customerID) FROM orders)
+
+------------------------------------------------------------------------------------------------------------------------------------------------------
+/*
++--------------+---------+
+| Column Name  | Type    |
++--------------+---------+
+| player_id    | int     |
+| device_id    | int     |
+| event_date   | date    |
+| games_played | int     |
++--------------+---------+
+(player_id, event_date) is the primary key of this table.
+This table shows the activity of players of some games.
+Each row is a record of a player who logged in and played a number of games (possibly 0) before logging out on someday using some device.
+ 
+
+Write an SQL query to report the first login date for each player.
+
+Return the result table in any order.           */
+
+SELECT player_id, MIN(event_date) as first_login
+FROM activity
+GROUP BY player_id
+
+-- if we do not use GROUP BY it returns the player with earliest first_login
+
+------------------------------------------------------------------------------------------------------------------------------------------------------
+/*
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| name        | varchar |
+| referee_id  | int     |
++-------------+---------+
+id is the primary key column for this table.
+Each row of this table indicates the id of a customer, their name, and the id of the customer who referred them.
+ 
+
+Write an SQL query to report the names of the customer that are not referred by the customer with id = 2.
+
+Return the result table in any order.                */
+
+SELECT name 
+FROM customer
+WHERE referee_id <> 2 OR referee_id IS NULL
+
+
+ 
 
 
 
