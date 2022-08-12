d0efb6e36dbb5b7c44644d22f064f69900a24874
Rajat Bahuguna <rajatbahuguna4445@gmail.com>
2022-08-12 06:36:00 +0530

    Update SQL_leetcode.sql

--- SQL_leetcode.sql
+++ SQL_leetcode.sql
@@ -6,6 +6,27 @@ FROM employee as e1, employee as e2
 WHERE e1.managerID = e2.id
 AND e1.salary > e2.salary
 
+----------------------------------------------------------------------------------------------------------------------------------------------------------
+/*
++-------------+---------+
+| Column Name | Type    |
+ +-------------+---------+
+| id          | int     |
+| email       | varchar |
++-------------+---------+
+id is the primary key column for this table.
+Each row of this table contains an email. The emails will not contain uppercase letters.
+ 
+
+Write an SQL query to report all the duplicate emails.
+
+Return the result table in any order. */
+
+SELECT email
+FROM person
+GROUP BY email
+HAVING COUNT(email) > 1
+
 
 
 
