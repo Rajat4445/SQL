021a8392e38048c43ffac3371bc906c851395cb9
Rajat Bahuguna <rajatbahuguna4445@gmail.com>
2022-08-13 06:15:43 +0530

    Update SQL_leetcode.sql

--- SQL_leetcode.sql
+++ SQL_leetcode.sql
@@ -182,10 +182,112 @@ WHERE area >= 3000000 OR population >= 25000000
 ------------------------------------------------------------------------------------------------------------------------------------------------------
 /*
 
-           
-
-
++----------------+----------+
+| Column Name    | Type     |
++----------------+----------+
+| id             | int      |
+| movie          | varchar  |
+| description    | varchar  |
+| rating         | float    |
++----------------+----------+
+id is the primary key for this table.
+Each row contains information about the name of a movie, its genre, and its rating.
+rating is a 2 decimal places float in the range [0, 10]
  
+
+Write an SQL query to report the movies with an odd-numbered ID and a description that is not "boring".
+
+Return the result table ordered by rating in descending order.         */
+
+SELECT *
+FROM cinema
+WHERE id%2 = 1 AND description <> 'boring'
+ORDER BY rating DESC
+
+------------------------------------------------------------------------------------------------------------------------------------------------------
+/*
+
++-------------+----------+
+| Column Name | Type     |
++-------------+----------+
+| id          | int      |
+| name        | varchar  |
+| sex         | ENUM     |
+| salary      | int      |
++-------------+----------+
+id is the primary key for this table.
+The sex column is ENUM value of type ('m', 'f').
+The table contains information about an employee.
+ 
+
+Write an SQL query to swap all 'f' and 'm' values (i.e., change all 'f' values to 'm' and vice versa) with a single update statement and no intermediate temporary tables.
+
+Note that you must write a single update statement, do not write any select statement for this problem.       */
+
+UPDATE salary
+SET sex = if(sex = 'm', 'f', 'm')
+
+------------------------------------------------------------------------------------------------------------------------------------------------------
+/*
+ +-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| actor_id    | int     |
+| director_id | int     |
+| timestamp   | int     |
++-------------+---------+
+timestamp is the primary key column for this table.
+ 
+
+Write a SQL query for a report that provides the pairs (actor_id, director_id) where the actor has cooperated with the director at least three times. */
+
+SELECT actor_id, director_id
+FROM actordirector
+GROUP BY actor_id, director_id
+HAVING COUNT(timestamp) >= 3
+
+------------------------------------------------------------------------------------------------------------------------------------------------------
+/*
+
+Table: Product
+
++--------------+---------+
+| Column Name  | Type    |
++--------------+---------+
+| product_id   | int     |
+| product_name | varchar |
+| unit_price   | int     |
++--------------+---------+
+product_id is the primary key of this table.
+Each row of this table indicates the name and the price of each product.
+Table: Sales
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| seller_id   | int     |
+| product_id  | int     |
+| buyer_id    | int     |
+| sale_date   | date    |
+| quantity    | int     |
+| price       | int     |
++-------------+---------+
+This table has no primary key, it can have repeated rows.
+product_id is a foreign key to the Product table.
+Each row of this table contains some information about one sale.
+ 
+
+Write an SQL query that reports the products that were only sold in the first quarter of 2019. That is, between 2019-01-01 and 2019-03-31 inclusive. */
+
+SELECT product_id , product_name
+FROM product
+WHERE product_id IN
+(SELECT product_id
+FROM sales
+GROUP BY product_id
+HAVING MIN(sale_date) BETWEEN '2019-01-01' AND '2019-03-31' AND MAX(sale_date) BETWEEN'2019-01-01' AND '2019-03-31')
+
+
 
 
 
