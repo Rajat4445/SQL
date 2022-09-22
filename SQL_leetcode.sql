dfa4e4fc665dd78c8c56a59bca51f752d581a630
Rajat Bahuguna <rajatbahuguna4445@gmail.com>
2022-09-22 19:56:03 +0530

    Update SQL_leetcode.sql

--- SQL_leetcode.sql
+++ SQL_leetcode.sql
@@ -368,8 +368,27 @@ DELETE p1 FROM Person p1,
 WHERE
     p1.Email = p2.Email AND p1.Id > p2.Id
 
+------------------------------------------------------------------------------------------------------------------------------------------------------
+/*
+Table: Users
 
++----------------+---------+
+| Column Name    | Type    |
++----------------+---------+
+| user_id        | int     |
+| name           | varchar |
++----------------+---------+
+user_id is the primary key for this table.
+This table contains the ID and the name of the user. The name consists of only lowercase and uppercase characters.
+ 
 
+Write an SQL query to fix the names so that only the first character is uppercase and the rest are lowercase.
+
+Return the result table ordered by user_id.                  */
+
+SELECT user_id, 
+               CONCAT(UCASE(SUBSTRING(name, 1,1)), LCASE(SUBSTRING(name, 2, LENGTH(name) + 1))) AS name
+FROM users
 
 
 
