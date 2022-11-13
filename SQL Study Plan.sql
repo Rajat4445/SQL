f655936adb60540a39ad50330b13383d0cd1af38
Rajat Bahuguna <rajatbahuguna4445@gmail.com>
2022-11-13 20:55:00 +0530

    Update SQL Study Plan.sql

--- SQL Study Plan.sql
+++ SQL Study Plan.sql
@@ -107,7 +107,151 @@ SELECT name AS customers FROM customers                                         
 WHERE id NOT IN (SELECT DISTINCT(customerID) FROM orders)
 
 
+/*
+Table: Employees
 
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| employee_id | int     |
+| name        | varchar |
+| salary      | int     |
++-------------+---------+
+employee_id is the primary key for this table.
+Each row of this table indicates the employee ID, employee name, and salary.
+ 
+
+Write an SQL query to calculate the bonus of each employee. The bonus of an employee is 100% of their salary if the ID of the employee is an odd number and the employee name does not start with the character 'M'. The bonus of an employee is 0 otherwise.
+
+Return the result table ordered by employee_id.                     */
+
+SELECT employee_id, 
+CASE 
+    WHEN employee_id%2 = 1 AND name NOT LIKE 'M%' THEN salary
+    ELSE 0*salary
+    END AS bonus
+    
+FROM employees
+ORDER BY employee_id
+
+/*
+Table: Salary
+
++-------------+----------+
+| Column Name | Type     |
++-------------+----------+
+| id          | int      |
+| name        | varchar  |
+| sex         | ENUM     |
+| salary      | int      |
++-------------+----------+
+id is the primary key for this table.
+The sex column is ENUM value of type ('m', 'f').
+The table contains information about an employee.
+ 
+
+Write an SQL query to swap all 'f' and 'm' values (i.e., change all 'f' values to 'm' and vice versa) with a single update statement and no intermediate temporary tables.
+
+Note that you must write a single update statement, do not write any select statement for this problem.                         */
+
+UPDATE Salary 
+SET sex = (CASE WHEN sex = 'f' THEN 'm' ELSE 'f' END) 
+
+/*
+Table: Person
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| email       | varchar |
++-------------+---------+
+id is the primary key column for this table.
+Each row of this table contains an email. The emails will not contain uppercase letters.
+ 
+
+Write an SQL query to delete all the duplicate emails, keeping only one unique email with the smallest id. Note that you are supposed to write a DELETE statement and not a SELECT one.
+
+After running your script, the answer shown is the Person table. The driver will first compile and run your piece of code and then show the Person table. The final order of the Person table does not matter.             */
+
+DELETE p1
+FROM Person p1, Person p2
+WHERE p1.Email = p2.Email AND
+p1.Id > p2.Id
+
+/*
+Table: Users
+
++----------------+---------+
+| Column Name    | Type    |
++----------------+---------+
+| user_id        | int     |
+| name           | varchar |
++----------------+---------+
+user_id is the primary key for this table.
+This table contains the ID and the name of the user. The name consists of only lowercase and uppercase characters.
+ 
+
+Write an SQL query to fix the names so that only the first character is uppercase and the rest are lowercase.
+
+Return the result table ordered by user_id.                                     */
+
+SELECT user_id,
+CONCAT(UPPER(SUBSTRING(name FROM 1 FOR 1)),LOWER(SUBSTRING(name FROM 2))) AS name
+FROM users
+ORDER BY user_id
+
+/*
+Table Activities:
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| sell_date   | date    |
+| product     | varchar |
++-------------+---------+
+There is no primary key for this table, it may contain duplicates.
+Each row of this table contains the product name and the date it was sold in a market.
+ 
+
+Write an SQL query to find for each date the number of different products sold and their names.
+
+The sold products names for each date should be sorted lexicographically.
+
+Return the result table ordered by sell_date.                                  */
+
+SELECT sell_date, COUNT(DISTINCT(product)) AS num_sold, 
+GROUP_CONCAT(DISTINCT(product) ORDER BY product) AS products
+FROM activities
+
+GROUP BY sell_date
+ORDER BY sell_date
+
+
+/*
+Table: Patients
+
++--------------+---------+
+| Column Name  | Type    |
++--------------+---------+
+| patient_id   | int     |
+| patient_name | varchar |
+| conditions   | varchar |
++--------------+---------+
+patient_id is the primary key for this table.
+'conditions' contains 0 or more code separated by spaces. 
+This table contains information of the patients in the hospital.
+ 
+
+Write an SQL query to report the patient_id, patient_name all conditions of patients who have Type I Diabetes. Type I Diabetes always starts with DIAB1 prefix
+
+Return the result table in any order.                         */
+
+SELECT * FROM patients
+WHERE conditions LIKE '% DIAB1%' OR conditions LIKE 'DIAB1%' -- The first condition takes care of the condition where DIAB1 is in between of the condition
+                                                             -- EX: SARDIAB1YHX 
+                                                             
+                                     
 
 
 
