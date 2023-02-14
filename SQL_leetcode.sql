4ce789f0891d4948373b147953215ab3e989bd2d
Rajat Bahuguna <rajatbahuguna4445@gmail.com>
2023-02-14 18:27:33 +0530

    Update SQL_leetcode.sql

--- SQL_leetcode.sql
+++ SQL_leetcode.sql
@@ -493,7 +493,49 @@ GROUP BY user_id
 
 
 
+/*
+Table: Employees
 
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| employee_id | int     |
+| name        | varchar |
++-------------+---------+
+employee_id is the primary key for this table.
+Each row of this table indicates the name of the employee whose ID is employee_id.
+ 
+
+Table: Salaries
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| employee_id | int     |
+| salary      | int     |
++-------------+---------+
+employee_id is the primary key for this table.
+Each row of this table indicates the salary of the employee whose ID is employee_id.
+ 
+
+Write an SQL query to report the IDs of all the employees with missing information. The information of an employee is missing if:
+
+The employee's name is missing, or
+The employee's salary is missing.
+Return the result table ordered by employee_id in ascending order.
+
+*/
+------------------------------------------------------------------------------------------------------------------------------------------------------ 
+SELECT e.employee_id FROM employees AS e 
+LEFT JOIN salaries AS s 
+ON e.employee_id = s.employee_id
+WHERE s.salary  is NULL
+UNION
+SELECT s.employee_id FROM salaries AS s
+LEFT JOIN employees AS e 
+ON e.employee_id = s.employee_id
+WHERE e.name is NULL
+ORDER BY employee_id;
 
 
 
