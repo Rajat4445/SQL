964346e0f3007320b92f44a43059ea5cb1f00a3a
Rajat Bahuguna <rajatbahuguna4445@gmail.com>
2023-02-20 21:04:52 +0530

    Update SQL Study Plan.sql

--- SQL Study Plan.sql
+++ SQL Study Plan.sql
@@ -295,8 +295,27 @@ ON e.employee_id = s.employee_id
 WHERE e.name is NULL
 ORDER BY employee_id;
 
+/*
+Table: Employee
+
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| id          | int  |
+| salary      | int  |
++-------------+------+
+id is the primary key column for this table.
+Each row of this table contains information about the salary of an employee.
+ 
+
+Write an SQL query to report the second highest salary from the Employee table. If there is no second highest salary, the query should report null.
 
 
+*/
+SELECT MAX(salary) AS SecondHighestSalary
+FROM employee
+WHERE salary != (SELECT MAX(Salary)
+                FROM employee)
 
 
 
