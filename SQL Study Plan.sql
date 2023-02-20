b19ce5f9d05eb9567a26c9bb9cdf428fd8e0a84f
Rajat Bahuguna <rajatbahuguna4445@gmail.com>
2023-02-20 20:51:11 +0530

    Update SQL Study Plan.sql

--- SQL Study Plan.sql
+++ SQL Study Plan.sql
@@ -253,7 +253,37 @@ WHERE conditions LIKE '% DIAB1%' OR conditions LIKE 'DIAB1%' -- The first condit
                                                              
                                      
 ------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+/*
+Table: Employees
 
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| employee_id | int     |
+| name        | varchar |
++-------------+---------+
+employee_id is the primary key for this table.
+Each row of this table indicates the name of the employee whose ID is employee_id.
+ 
+
+Table: Salaries
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| employee_id | int     |
+| salary      | int     |
++-------------+---------+
+employee_id is the primary key for this table.
+Each row of this table indicates the salary of the employee whose ID is employee_id.
+ 
+
+Write an SQL query to report the IDs of all the employees with missing information. The information of an employee is missing if:
+
+The employee's name is missing, or
+The employee's salary is missing.
+Return the result table ordered by employee_id in ascending order.*/
+---------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 
 
