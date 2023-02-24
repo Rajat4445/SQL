456bd91200635988a1937c0579f06e50dbe196e1
Rajat Bahuguna <rajatbahuguna4445@gmail.com>
2023-02-24 21:11:24 +0530

    Update SQL Study Plan.sql

--- SQL Study Plan.sql
+++ SQL Study Plan.sql
@@ -564,3 +564,21 @@ FROM orders o
 LEFT JOIN company c
 ON o.com_id = c.com_id
 WHERE c.name LIKE 'RED')
+
+/*
+Table: Person
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| email       | varchar |
++-------------+---------+
+id is the primary key column for this table.
+Each row of this table contains an email. The emails will not contain uppercase letters.
+ 
+
+Write an SQL query to report all the duplicate emails. Note that it's guaranteed that the email field is not NULL.
+
+Return the result table in any order.
+*/
