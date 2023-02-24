7774cb69c08d057b8c7a1ab2628d30c71778bc21
Rajat Bahuguna <rajatbahuguna4445@gmail.com>
2023-02-24 19:39:59 +0530

    Update SQL Study Plan.sql

--- SQL Study Plan.sql
+++ SQL Study Plan.sql
@@ -373,3 +373,14 @@ Write an SQL query to rearrange the Products table so that each row has (product
 
 Return the result table in any order.
 */
+SELECT product_id, 'store1' AS store, store1 AS price
+FROM products
+WHERE store1 IS NOT NULL
+UNION
+SELECT product_id, 'store2' AS store, store2 AS price
+FROM products
+WHERE store2 IS NOT NULL
+UNION
+SELECT product_id, 'store3' AS store, store3 AS price
+FROM products
+WHERE store3 IS NOT NULL
