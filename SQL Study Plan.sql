9debfd964fa0ececd5ea27e1c857f43fd65ea6d1
Rajat Bahuguna <rajatbahuguna4445@gmail.com>
2023-02-24 20:12:01 +0530

    Update SQL Study Plan.sql

--- SQL Study Plan.sql
+++ SQL Study Plan.sql
@@ -483,3 +483,22 @@ SELECT DISTINCT(author_id) as id
 FROM views
 WHERE author_id = viewer_id
 ORDER BY id
+
+/*
+Table: Weather
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| id            | int     |
+| recordDate    | date    |
+| temperature   | int     |
++---------------+---------+
+id is the primary key for this table.
+This table contains information about the temperature on a certain day.
+ 
+
+Write an SQL query to find all dates' Id with higher temperatures compared to its previous dates (yesterday).
+
+Return the result table in any order.
+*/
