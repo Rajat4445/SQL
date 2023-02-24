ee24cc27650ac43cedfff852d4ac157ccbed424a
Rajat Bahuguna <rajatbahuguna4445@gmail.com>
2023-02-24 20:02:39 +0530

    Update SQL Study Plan.sql

--- SQL Study Plan.sql
+++ SQL Study Plan.sql
@@ -421,3 +421,40 @@ SELECT firstName, lastname, city, state
 FROM person
 LEFT JOIN address
 ON person.personid = address.personid
+
+/*
+Table: Visits
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| visit_id    | int     |
+| customer_id | int     |
++-------------+---------+
+visit_id is the primary key for this table.
+This table contains information about the customers who visited the mall.
+ 
+
+Table: Transactions
+
++----------------+---------+
+| Column Name    | Type    |
++----------------+---------+
+| transaction_id | int     |
+| visit_id       | int     |
+| amount         | int     |
++----------------+---------+
+transaction_id is the primary key for this table.
+This table contains information about the transactions made during the visit_id.
+ 
+
+Write a SQL query to find the IDs of the users who visited without making any transactions and the number of times they made these types of visits.
+
+Return the result table sorted in any order.
+*/
+SELECT customer_id, COUNT(visits.visit_id) AS 'count_no_trans'
+FROM visits
+LEFT JOIN transactions
+ON visits.visit_id = transactions.visit_id
+WHERE transactions.visit_id IS NULL
+GROUP BY customer_id
