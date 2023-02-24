c80cc8d803c4c1514b0848f04dc08b8eff8178b6
Rajat Bahuguna <rajatbahuguna4445@gmail.com>
2023-02-24 20:04:02 +0530

    Update SQL Study Plan.sql

--- SQL Study Plan.sql
+++ SQL Study Plan.sql
@@ -458,3 +458,28 @@ LEFT JOIN transactions
 ON visits.visit_id = transactions.visit_id
 WHERE transactions.visit_id IS NULL
 GROUP BY customer_id
+
+/*
+Table: Views
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| article_id    | int     |
+| author_id     | int     |
+| viewer_id     | int     |
+| view_date     | date    |
++---------------+---------+
+There is no primary key for this table, it may have duplicate rows.
+Each row of this table indicates that some viewer viewed an article (written by some author) on some date. 
+Note that equal author_id and viewer_id indicate the same person.
+ 
+
+Write an SQL query to find all the authors that viewed at least one of their own articles.
+
+Return the result table sorted by id in ascending order.
+*/
+SELECT DISTINCT(author_id) as id
+FROM views
+WHERE author_id = viewer_id
+ORDER BY id
