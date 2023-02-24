dcc0cf88e40ef662a3942aee5edb9638ca6cf706
Rajat Bahuguna <rajatbahuguna4445@gmail.com>
2023-02-24 21:18:17 +0530

    Update SQL Study Plan.sql

--- SQL Study Plan.sql
+++ SQL Study Plan.sql
@@ -587,7 +587,23 @@ FROM person
 GROUP BY email
 HAVING COUNT(email) != 1
 
+/*
+Table: ActorDirector
 
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| actor_id    | int     |
+| director_id | int     |
+| timestamp   | int     |
++-------------+---------+
+timestamp is the primary key column for this table.
+ 
+
+Write a SQL query for a report that provides the pairs (actor_id, director_id) where the actor has cooperated with the director at least three times.
+
+Return the result table in any order.
+*/
 
 SELECT actor_id, director_id
 FROM actordirector
