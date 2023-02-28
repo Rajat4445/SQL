1b27386b8ecab7b307e841d12c2ed907f6bb4333
Rajat Bahuguna <rajatbahuguna4445@gmail.com>
2023-02-28 19:35:07 +0530

    Update SQL_leetcode.sql

--- SQL_leetcode.sql
+++ SQL_leetcode.sql
@@ -578,3 +578,7 @@ Write an SQL query that will, for each user, return the number of followers.
 
 Return the result table ordered by user_id in ascending order.*/
 
+SELECT user_id, COUNT(follower_id) AS followers_count
+FROM followers
+GROUP BY user_id
+ORDER BY user_id
