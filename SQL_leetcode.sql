91a11f8ec4fd3ec2e14c401e41d3b2279e4adbda
Rajat Bahuguna <rajatbahuguna4445@gmail.com>
2023-02-28 20:09:46 +0530

    Update SQL_leetcode.sql

--- SQL_leetcode.sql
+++ SQL_leetcode.sql
@@ -608,3 +608,18 @@ Return the result table in any order. */
 SELECT event_day AS day, emp_id, SUM(out_time-in_time) AS total_time
 FROM employees
 GROUP BY day, emp_id
+
+/*
+Table: Employee
+
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| id          | int  |
+| salary      | int  |
++-------------+------+
+id is the primary key column for this table.
+Each row of this table contains information about the salary of an employee.
+ 
+
+Write an SQL query to report the nth highest salary from the Employee table. If there is no nth highest salary, the query should report null. */
