90da388ba8a3062499502fca690536c4a36e900f
Rajat Bahuguna <rajatbahuguna4445@gmail.com>
2023-03-05 20:29:06 +0530

    Update SQL Study Plan.sql

--- SQL Study Plan.sql
+++ SQL Study Plan.sql
@@ -775,3 +775,9 @@ This table contains information about the order ID and the customer ID.
 Write an SQL query to find the customer_number for the customer who has placed the largest number of orders.
 
 The test cases are generated so that exactly one customer will have placed more orders than any other customer. */
+
+SELECT customer_number FROM (SELECT customer_number, COUNT(order_number)
+FROM orders
+GROUP BY customer_number
+ORDER BY COUNT(order_number) DESC
+LIMIT 1) AS a
