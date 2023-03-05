a0014bb6e8adea01a30713a491beeab768da4508
Rajat Bahuguna <rajatbahuguna4445@gmail.com>
2023-03-05 20:32:18 +0530

    Update SQL Study Plan.sql

--- SQL Study Plan.sql
+++ SQL Study Plan.sql
@@ -802,3 +802,6 @@ Write an SQL query to report the first login date for each player.
 
 Return the result table in any order.     */
 
+SELECT player_id, MIN(event_date) AS first_login
+FROM activity
+GROUP BY player_id
