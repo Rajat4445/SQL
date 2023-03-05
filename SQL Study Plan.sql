875e3b8a8d9a47dca7534cb9231fa0f40b0f1c35
Rajat Bahuguna <rajatbahuguna4445@gmail.com>
2023-03-05 20:20:44 +0530

    Update SQL Study Plan.sql

--- SQL Study Plan.sql
+++ SQL Study Plan.sql
@@ -759,4 +759,19 @@ GROUP BY rides.user_id
 ORDER BY travelled_distance DESC, name ASC
 
 
+/*
+Table: Orders
 
++-----------------+----------+
+| Column Name     | Type     |
++-----------------+----------+
+| order_number    | int      |
+| customer_number | int      |
++-----------------+----------+
+order_number is the primary key for this table.
+This table contains information about the order ID and the customer ID.
+ 
+
+Write an SQL query to find the customer_number for the customer who has placed the largest number of orders.
+
+The test cases are generated so that exactly one customer will have placed more orders than any other customer. */
