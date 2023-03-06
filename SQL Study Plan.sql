854e78966a3e3e9a18dc4d8d3f1701b151ff8abd
Rajat Bahuguna <rajatbahuguna4445@gmail.com>
2023-03-06 06:38:13 +0530

    Update SQL Study Plan.sql

--- SQL Study Plan.sql
+++ SQL Study Plan.sql
@@ -899,3 +899,10 @@ All accounts start with a balance of 0.
 Write an SQL query to report the name and balance of users with a balance higher than 10000. The balance of an account is equal to the sum of the amounts of all transactions involving that account.
 
 Return the result table in any order.   */
+
+SELECT name, SUM(amount) AS balance
+FROM users
+LEFT JOIN transactions
+ON users.account = transactions.account
+GROUP BY name
+HAVING SUM(amount) > 10000
