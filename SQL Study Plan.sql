4ac02c180eeb1fca6cf48888a8224dc40aed257b
Rajat Bahuguna <rajatbahuguna4445@gmail.com>
2023-03-06 06:39:53 +0530

    Update SQL Study Plan.sql

--- SQL Study Plan.sql
+++ SQL Study Plan.sql
@@ -940,3 +940,10 @@ Write an SQL query that reports the products that were only sold in the first qu
 
 Return the result table in any order.       */
 
+SELECT p.product_id, p.product_name 
+FROM product p
+JOIN sales s
+    USING (product_id)
+GROUP BY s.product_id 
+HAVING MIN(s.sale_date) >= "2019-01-01" 
+    AND MAX(s.sale_date) <= "2019-03-31"
