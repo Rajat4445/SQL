88b566b00f64201cc7ff11d5a010e6b59ceb1b75
Rajat Bahuguna <rajatbahuguna4445@gmail.com>
2023-03-09 21:15:44 +0530

    Update SQL_leetcode.sql

--- SQL_leetcode.sql
+++ SQL_leetcode.sql
@@ -18,7 +18,7 @@ id is the primary key column for this table.
 Each row of this table contains an email. The emails will not contain uppercase letters
  
 
-Write an SQL query to report all the duplicate emails.
+Write an SQL query to report all the duplicate emails
 
 Return the result table in any order */
 
