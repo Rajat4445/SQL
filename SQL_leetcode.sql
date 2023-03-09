8fbaeb4c4def393e15f2ce4822f9f327c81ae6a8
Rajat Bahuguna <rajatbahuguna4445@gmail.com>
2023-03-09 21:15:22 +0530

    Update SQL_leetcode.sql

--- SQL_leetcode.sql
+++ SQL_leetcode.sql
@@ -15,7 +15,7 @@ AND e1.salary > e2.salary
 | email       | varchar |
 +-------------+---------+
 id is the primary key column for this table.
-Each row of this table contains an email. The emails will not contain uppercase letters.
+Each row of this table contains an email. The emails will not contain uppercase letters
  
 
 Write an SQL query to report all the duplicate emails.
